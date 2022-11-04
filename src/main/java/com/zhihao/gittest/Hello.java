b5b46c77438999e691b93785960ffb60327fcb23
Merlin <1006308848@qq.com>
2022-11-04 22:58:58 +0800

    fourth commit

--- src/main/java/com/zhihao/gittest/Hello.java
+++ src/main/java/com/zhihao/gittest/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git master test");
         System.out.println("Hello Git");
     }
 }
