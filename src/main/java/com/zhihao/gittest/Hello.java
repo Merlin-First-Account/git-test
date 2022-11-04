be8b961847099bb0b11799943a9e9594363d5562
Merlin <1006308848@qq.com>
2022-11-04 22:45:22 +0800

    second commit

--- src/main/java/com/zhihao/gittest/Hello.java
+++ src/main/java/com/zhihao/gittest/Hello.java
@@ -3,5 +3,6 @@ package com.zhihao.gittest;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
