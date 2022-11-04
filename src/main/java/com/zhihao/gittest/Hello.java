d2423345f9d78d4f042c3e0b5e17456fa59abf6c
Merlin <1006308848@qq.com>
2022-11-04 23:03:53 +0800

    master commit3

--- src/main/java/com/zhihao/gittest/Hello.java
+++ src/main/java/com/zhihao/gittest/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("Hello Git");
         System.out.println("Hello Git master test");
         System.out.println("Hello Git hot-fix test");
+        System.out.println("Hello Git");
     }
 }
